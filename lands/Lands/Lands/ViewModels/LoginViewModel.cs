72a3b293fcd008e1961154df1ea60727ae1a607e
magora84 <magora84@hotmail.com>
2018-02-12 00:51:23 -0600

    loginViewModel

---
+++ lands/Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Lands.ViewModels
+{
+    using System.Windows.Input;
+    public class LoginViewModel
+    {
+        #region Propiedades
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsRunning { get; set; }
+        public bool IsRemembered { get; set; }
+        #endregion
+
+        public LoginViewModel() {
+            this.IsRemembered = true;
+
+        }
+        #region Commandos
+        public ICommand LoginCommand { get; set; }
+        #endregion
+
+    }
+}
--- lands/Lands/Lands/ViewModels/MainViewModel.cs
+++ lands/Lands/Lands/ViewModels/MainViewModel.cs
@@ -6,5 +6,14 @@ namespace Lands.ViewModels
 {
    public class MainViewModel
     {
+        #region ViewModels 
+        public LoginViewModel Login { get; set; }
+        #endregion
+
+        #region Constructors
+        public MainViewModel() {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
