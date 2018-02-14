6cf84c699a7bfada1954065309ee886125ba4f41
magora84 <magora84@hotmail.com>
2018-02-14 01:33:52 -0600

    Respuesta correcta del web service

--- lands/Lands/Lands/ViewModels/LansViewModel.cs
+++ lands/Lands/Lands/ViewModels/LansViewModel.cs
@@ -1,11 +1,49 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Lands.ViewModels
 {
-   public class LansViewModel
-    {
+    using Servicios;
+    using Models;
+    using System.Collections.ObjectModel;
+    using Xamarin.Forms;
+    public class LansViewModel : BaseViewModel {
+        #region servicios
+        private ApiService apiService;
 
+        #endregion
+
+        #region Atributos
+        private ObservableCollection<Land> lands;
+
+        #endregion
+
+        #region propiedades
+        public ObservableCollection <Land> Lands {
+            get {
+                return this.lands;
+            } set { SetValue(ref this.lands, value); } }
+        #endregion
+        #region constructor
+        public LansViewModel() {
+            this.apiService = new ApiService();
+            this.LoadLands();
+        }
+        #endregion
+
+        #region metodos
+        private async void LoadLands() {
+
+            var response = await this.apiService.GetList<Land>("http://restcountries.eu", "/rest", "/v2/all");
+            if (!response.IsSuccess) {
+                await Application.Current.MainPage.DisplayAlert("error", response.Message, "aceptar");
+                return;
+            }
+            var list = (List<Land>)response.Result;
+            this.Lands = new ObservableCollection<Land>(list);
+        } 
+        #endregion
     }
 }
