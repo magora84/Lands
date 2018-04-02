5ed89f23d3e8961c6b0cb719a130af3b88e02900
magora84 <magora84@hotmail.com>
2018-04-02 09:15:56 -0500

    actualizacion sqlite

---
+++ lands/Lands/Lands/Interfaces/IConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite.Net.Interop;
+namespace Lands.Interfaces
+{
+  public  interface IConfig
+    {
+         string DirectoryDB { get;  }
+        ISQLitePlatform Platform { get; }
+    }
+}
